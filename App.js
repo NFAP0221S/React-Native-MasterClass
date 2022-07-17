22f7a4ad93cc5c1c7fba81d45c266a03a1da36ac
katf101 <vawav11@gmail.com>
2022-07-17 23:21:37 +0900

    AppLoading

---
+++ App.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import { Text } from "react-native";
+import AppLoading from "expo-app-loading";
+
+export default function App() {
+  const [ready, setReady] = useState(false);
+  const onFinish = () => setReady(true);
+  const startLoading = async () => {
+    await new Promise((resolve) => setTimeout(resolve, 10000));
+  };
+  if (!ready) {
+    return (
+      <AppLoading
+        startAsync={startLoading}
+        onFinish={onFinish}
+        onError={console.error}
+      />
+    );
+  }
+  return <Text>We are done loading!</Text>;
+}
