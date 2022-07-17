d99ee2da84a4741aa58701275ec984a38df83e30
katf101 <vawav11@gmail.com>
2022-07-18 01:04:02 +0900

    preload ( font, asset)

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
-import React, { useState } from "react";
-import { Text } from "react-native";
 import AppLoading from "expo-app-loading";
+import React, { useState } from "react";
+import * as Font from "expo-font";
+import { Asset } from "expo-asset";
+import { Text, Image } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function App() {
   const [ready, setReady] = useState(false);
   const onFinish = () => setReady(true);
   const startLoading = async () => {
-    await new Promise((resolve) => setTimeout(resolve, 10000));
+    await Font.loadAsync(Ionicons.font);
+    await Asset.loadAsync(require("./image2.jpg"));
+    await Image.prefetch(
+      "https://image.wanted.co.kr/optimize?src=https%3A%2F%2Fstatic.wanted.co.kr%2Fimages%2Fbanners%2F1756%2Fe89a0686.thumb_1006_280.jpg&w=1060&q=100"
+    );
   };
   if (!ready) {
     return (
