c0c3828338f5068f82ba5d1dd17c19082f10f210
katf101 <vawav11@gmail.com>
2022-07-18 10:46:01 +0900

    #2.3 preload, hooks

--- App.js
+++ App.js
@@ -1,40 +1,15 @@
 import AppLoading from "expo-app-loading";
 import React, { useState } from "react";
 import * as Font from "expo-font";
-import { Asset } from "expo-asset";
+import { Asset, useAssets } from "expo-asset";
 import { Text, Image } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
-const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
-
-const loadImage = (images) =>
-  images.map((image) => {
-    if (typeof image === "string") {
-      return Image.prefetch(image);
-    } else {
-      return Asset.loadAsync(image);
-    }
-  });
-
 export default function App() {
-  const [ready, setReady] = useState(false);
-  const onFinish = () => setReady(true);
-  const startLoading = async () => {
-    const fonts = loadFonts([Ionicons.font]);
-    const images = loadImage([
-      require("./crypto.png"),
-      "https://nomadcoders.co/m.svg",
-    ]);
-    await Promise.all([...fonts, ...images]);
-  };
-  if (!ready) {
-    return (
-      <AppLoading
-        startAsync={startLoading}
-        onFinish={onFinish}
-        onError={console.error}
-      />
-    );
+  const [assets] = useAssets([require("./crypto.png")]);
+  const [loaded] = Font.useFonts(Ionicons.font);
+  if (!assets || !loaded) {
+    return <AppLoading />;
   }
   return <Text>We are done loading!</Text>;
 }
